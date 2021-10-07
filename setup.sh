81dc916349e3e4c7d5ac5b6250c105639d78b0df
I'm Unix <ferdi@ferdiwardiyan.eu.org>
2021-10-07 21:36:11 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt-get install --no-install-recommends ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal gnome-core wget unzip
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+./ngrok authtoken 1y7j8abTyQtrGWZskLeAK8r0I3b_3o9WLusuQzTMeKYKhGMV4
+sudo apt install vnc4server
